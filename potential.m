d4600222e636bfbd61058a73569679e30632e620
Denise Tumiotto <denise.tumiotto@mathematik.uni-halle.de>
2022-04-29 15:31:40 +0200

    rewrite potential (just sign changes)

--- potential.m
+++ potential.m
@@ -4,9 +4,9 @@ function val = potential(q, L, m)
 % subject to Earth gravity [g = 9.81 m/s^2]
 % with fix end in [0, 0, 0]
 
-g = -9.81;
+g = 9.81;
 e3 = [0; 0; 1];
 
-val = - g * m * e3' * q - L * g * m;
+val = g * m * e3' * q + L * g * m;
 
 end
