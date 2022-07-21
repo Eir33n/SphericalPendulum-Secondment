a9c11f4e788d155c606e974c8b377188afedab1b
Denise Tumiotto <denise.tumiotto@gmail.com>
2022-07-21 15:43:59 +0200

    adding damping

--- freeRigidBody.m
+++ freeRigidBody.m
@@ -1,9 +1,14 @@
-function yPrime = freeRigidBody(y, invInertia)
+function yPrime = freeRigidBody(y, invInertia, alpha)
+
+if nargin == 2
+    alpha = 0;
+end
+
 m = y(1:3);
 Q = y(4:3+9);
 Q = reshape(Q, 3, 3);
 
-mPrime = cross(m, invInertia * m);
+mPrime = cross(m, invInertia * m) - alpha * m;
 QPrime = Q * skw(invInertia * m);
 QPrime = reshape(QPrime, 9, 1);
 
--- main_frb.m
+++ main_frb.m
@@ -14,8 +14,7 @@ if I2 > I1
 end
 I3 = 1;
 
-inertia = diag([I1, I2, I3]);
-invInertia = inv(inertia);
+inertia = diag([1/I1, 1/I2, 1/I3]);
 
 % initial angular momentum
 m0 = rand(3, 1);
@@ -27,6 +26,8 @@ Q = reshape(Q, 9, 1);
 
 y0 = [m0; Q];
 
+alpha = 0.1;
+
 %% numerical parameters
 
 t0 = 0;
@@ -36,9 +37,45 @@ rtol = 1e-6;
 
 %% equations
 
-f = @(t, y) freeRigidBody(y, invInertia);
+if exist('alpha','var')
+    f = @(t, y) freeRigidBody(y, inertia, alpha);
+else
+    f = @(t, y) freeRigidBody(y, inertia);
+end
 
 %% solution
 
 options = odeset('AbsTol', atol, 'RelTol', rtol);
 ySol = ode45(f, [t0, te], y0);
+
+nTime = size(ySol.x, 2);
+time = linspace(t0, te, nTime);
+zSol = ySol.y;
+mSol = deval(ySol, time, 1:3);
+QSol = deval(ySol, time, 3 + (1:9));
+
+%% energy evaluation
+
+kinEnergy = zeros(nTime, 1);
+energy = @(m) 0.5 * m' * inertia * m;
+
+for i = 1:nTime
+    kinEnergy(i) = energy(mSol(:, i));
+end
+
+%% plots
+
+figure()
+plot(time, mSol(1, :), time, mSol(2, :), time, mSol(3, :), LineWidth=1.5)
+legend('mx','my','mz')
+grid on
+title('Angular Momenta', 'FontSize', 20)
+xlabel('time', FontSize=16)
+ylabel('angular momenta', FontSize=16)
+
+figure()
+plot(time, kinEnergy, LineWidth=1.5)
+grid on
+title('Kinetic Energy', 'FontSize', 20)
+xlabel('time', FontSize=16)
+ylabel('Kinetic energy', FontSize=16)
